4ffb873aa2876e75acc71feeb36f6ba01b7085ec
nikolaia <nikolaia@users.noreply.github.com>
2015-11-22 17:52:35 +0100

    Add homebrew file for clean install. Change gitconfig to use gxargs (from findutils) instead of xargs.

---
+++ dotfiles/hombebrew.fish
@@ -0,0 +1,28 @@
+# Normal brew
+brew install findutils
+brew install android-sdk
+brew install fish
+brew install node
+brew install watchman
+brew install flow
+brew install z
+
+# Cask
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install lastpass
+brew cask install sublime-text
+brew cask install atom
+brew cask install visual-studio-code
+brew cask install dropbox
+brew cask install vlc
+brew cask install moom
+brew cask install microsoft-office
+brew cask install beamer
+brew cask install transmission
+brew cask install spotify
+brew cask install skype
+brew cask install slack
+brew cask install discord
+brew cask install evernote
+brew cask install sourcetree
