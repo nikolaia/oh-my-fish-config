b5fd815f804f7c885e7f167ab6a089950525b62f
nikolaia <nikolai@stasj.com>
2019-09-25 16:42:04 +0200

    Use vi mode in fish

--- before.init.fish
+++ before.init.fish
@@ -5,6 +5,13 @@ set -g theme_complete_path yes
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 
+function fish_user_key_bindings
+    bind -M default \$ end-of-line accept-autosuggestion
+end
+
 function fish_greeting
     fortune -a
 end
+
+fish_vi_key_bindings
+set -g fish_escape_delay_ms 10
